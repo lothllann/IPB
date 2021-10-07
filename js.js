15a0d362277671ebfe769d224a36382cfd2c3ca4
lothllann <nick.ruppenthal@gmail.com>
2021-10-07 16:27:48 -0300

    finalemte botei JS

--- js.js
+++ js.js
@@ -1,5 +1,27 @@
+const botaoAbrir = document.querySelector('[data-modal="abrir"]');
+const botaoFechar = document.querySelector('[data-modal="fechar"]');
+const containerModal = document.querySelector('[data-modal="container"]');
+const btnSend = document.querySelector('[data-modal="enviar"]');
 
+if (botaoAbrir && botaoFechar && containerModal && btnSend) {
 
-('.btn').click(function () {
-  (this).toggleClass('active');
-});
+    function enviar() {
+        preventDefault();
+    }
+
+    function toggleModal(event) {
+        event.preventDefault();
+        containerModal.classList.toggle('ativo');
+    }
+
+    function cliqueForaModal(event) {
+        if (event.target === this) {
+            toggleModal(event);
+        }
+    }
+
+    botaoAbrir.addEventListener('click', toggleModal);
+    botaoFechar.addEventListener('click', toggleModal);
+    containerModal.addEventListener('click', cliqueForaModal);
+    btnSend.addEventListener('click', enviar);
+}
