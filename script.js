13b3c202e4bbfed80b42fc3c6ee2103ada3f8455
lothllann <nick.ruppenthal@gmail.com>
2021-10-13 16:50:20 -0300

    link simbolos de fé

--- script.js
+++ script.js
@@ -3,28 +3,32 @@ const botaoFechar = document.querySelector('[data-modal="fechar"]');
 const containerModal = document.querySelector('[data-modal="container"]');
 const btnSend = document.querySelector('[data-modal="enviar"]');
 
-if (botaoAbrir && botaoFechar && containerModal && btnSend) {
 
-    function enviar() {
-        preventDefault();
-    }
 
-    function toggleModal(event) {
-        event.preventDefault();
-        containerModal.classList.toggle('ativo');
-    }
-
-    function cliqueForaModal(event) {
-        if (event.target === this) {
-            toggleModal(event);
-        }
-    }
-
-    botaoAbrir.addEventListener('click', toggleModal);
-    botaoFechar.addEventListener('click', toggleModal);
-    containerModal.addEventListener('click', cliqueForaModal);
-    btnSend.addEventListener('click', enviar);
+function enviar() {
+    preventDefault();
 }
+
+function toggleModal(event) {
+    event.preventDefault();
+    containerModal.classList.toggle('ativo');
+}
+
+function cliqueForaModal(event) {
+    if (event.target === this) {
+        toggleModal(event);
+    }
+}
+
+botaoAbrir.addEventListener('click', toggleModal);
+botaoFechar.addEventListener('click', toggleModal);
+containerModal.addEventListener('click', cliqueForaModal);
+btnSend.addEventListener('click', enviar);
+
+
+
+/*---------------------------------------------------------------------------------------------------------------
+ */
 
 
 const menu = document.querySelector('[data-menu="scroll"]');
@@ -45,3 +49,19 @@ function showMenu() {
 
 
 window.addEventListener('scroll', showMenu);
+
+/*---------------------------------------------------------------------------------------------------------------
+ */
+
+// const logo_preto = document.querySelector('[data-menu="lp"]');
+// const logo_roxo = document.querySelector('[data-menu="lr"]');
+
+// function toggleMenu(event) {
+//     event.preventDefault();
+//     logo_preto.classList.toggle('ativo');
+//     logo_roxo.classList.toggle('ativo');
+
+// }
+
+// logo_preto.addEventListener('mouseover', toggleMenu);
+// logo_roxo.addEventListener('mouseup', 'mousedown', toggleMenu);
